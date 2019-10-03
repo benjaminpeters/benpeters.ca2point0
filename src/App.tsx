080b63b9954be19cc879b49c4950d9edd403070a
Benjamin <benpeters49@gmail.com>
2019-10-02 21:41:50 -0400

    Update background image

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import imageUrl from "./assets/images/Mojave Day.jpg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App backgroundImage"></div>;
 }
 
 export default App;
