3f6f8978752112819da4e6c42b35218f6d0a43b8
Benjamin <benpeters49@gmail.com>
2019-10-08 19:47:42 -0400

    Added logo to top menu

--- src/components/TopMenu/index.tsx
+++ src/components/TopMenu/index.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./TopMenu.css";
 
+import bigBen from "../../assets/images/icons/fox.svg";
+
 export default class extends React.Component<props> {
   render() {
     return (
       <div className="container">
-        <div className="menuText">Benjamin</div>
+        <img className="bigBen" src={bigBen} />
+
+        <div className="menuText menuTextBold">Benjamin</div>
+        <div className="menuText">Welcome</div>
+        <div className="menuText">To</div>
+        <div className="menuText">My</div>
+        <div className="menuText">Site</div>
       </div>
     );
   }
